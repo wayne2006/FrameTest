33f8e05a5e4422ccb11f9a0aa7b14424c9da32fc
Wayne <25139198@qq.com>
2019-10-02 21:24:53 +0800

    添加项目文件。

---
+++ FrameTest/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using LibVLCSharp.Shared;
+
+namespace FrameTest
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            Core.Initialize();
+        }
+    }
+}
---
+++ FrameTest/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using LibVLCSharp.Shared;
+
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using MediaPlayer = LibVLCSharp.Shared.MediaPlayer;
+
+namespace FrameTest
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        LibVLC _libVLC;
+        MediaPlayer _mediaPlayer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _libVLC = new LibVLC();
+            _mediaPlayer = new MediaPlayer(_libVLC);
+
+            //VideoView.Loaded += (sender, e) => VideoView.MediaPlayer = _mediaPlayer;
+            VideoView.Loaded += (sender, e) => {
+
+                VideoView.MediaPlayer = _mediaPlayer;
+
+                if (!VideoView.MediaPlayer.IsPlaying)
+                {
+                    VideoView.MediaPlayer.Play(new Media(_libVLC,
+                        @"FrameTest.mp4", FromType.FromPath));
+                }
+            };
+
+            
+        }
+    }
+}
